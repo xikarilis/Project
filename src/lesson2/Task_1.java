87889136d9500119b64cb20fc1baf9323dd6a170
xikari <xikari.98@mail.ru>
2022-01-20 22:07:12 +0300

    initial commit

--- src/lesson2/Task_1.java
+++ src/lesson2/Task_1.java
@@ -10,12 +10,35 @@ public class Task_1 {
         int secondNumber = scTask_1.nextInt();
         int thirdNumber = scTask_1.nextInt();
 
-        if (firstNumber > secondNumber){
+//        if (secondNumber == thirdNumber) {
+//            if(secondNumber == firstNumber){
+//                System.out.println("the numbers are equal");
+//            }}
+//        if (firstNumber > secondNumber) {
+//            if (firstNumber > thirdNumber) {
+//                System.out.println("first number " + firstNumber);
+//            }}
+//        if (secondNumber > firstNumber) {
+//            if (secondNumber > thirdNumber){
+//                System.out.println("second number " + secondNumber);
+//            }}
+//        if (thirdNumber>=firstNumber){
+//            if (thirdNumber>secondNumber){
+//                System.out.println("third number " + thirdNumber);
+//            }}
+        if (firstNumber == secondNumber && secondNumber == thirdNumber){
+            System.out.println("the numbers are equal");
+        }
+        if (firstNumber > secondNumber && firstNumber > thirdNumber){
             System.out.println("first number " + firstNumber);
-        if (secondNumber > thirdNumber)
+        }
+        if (secondNumber > firstNumber && secondNumber > thirdNumber){
             System.out.println("second number " + secondNumber);
-        if (thirdNumber>secondNumber)
-            System.out.println("");
+        }
+        if (thirdNumber > secondNumber && thirdNumber > firstNumber){
+            System.out.println("third number " + thirdNumber);
+        }
         }
     }
-}
+
+
