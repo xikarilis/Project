2faffa9c228f13f1ee13ecbbe58cd13c83fe8b6c
xikari <xikari.98@mail.ru>
2022-01-20 22:52:54 +0300

    initial commit

--- src/lesson2/Task_2.java
+++ src/lesson2/Task_2.java
@@ -1,4 +1,16 @@
 package lesson2;
 
+import java.util.Scanner;
+
 public class Task_2 {
+    public static void main(String[] args) {
+        Scanner scTask_2 = new Scanner(System.in);
+        double firstNumber = scTask_2.nextDouble();
+        double secondNumber = scTask_2.nextDouble();
+        if (firstNumber%secondNumber == 0){
+            System.out.println("кратное");
+        }else {
+            System.out.println("не кратное");
+        }
+    }
 }
