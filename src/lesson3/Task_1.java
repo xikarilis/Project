37dbfc8c715cfaeab2021534bf7df5baec7527b0
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-22 22:02:08 +0300

    Update Task_1.java

--- src/lesson3/Task_1.java
+++ src/lesson3/Task_1.java
@@ -2,6 +2,7 @@ package lesson3;
 
 public class Task_1 {
     public static void main(String[] args) {
+        //why did you put amoeba in for? do we need to cycle this variable? Or use inner for?
         for (int amoeba = 1, time = 0; time <= 24; time += 3) {
             System.out.println(amoeba *=2);
         }
