881e4e014be68dfbad2935550d1c7993366b4fe8
xikari <xikari.98@mail.ru>
2022-01-27 21:46:47 +0300

    initial commit

--- src/lesson3/ClassWork.java
+++ src/lesson3/ClassWork.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class ClassWork {
     public static void main(String[] args) {
-//        Scanner scClassWork = new Scanner(System.in);
-//        int n = scClassWork.nextInt();
-//        int res = 0;
-//        for (int i = 0; i <= n; i++) {
-//                res+=i;
-//        }
-//        System.out.println(res);
+        Scanner scClassWork = new Scanner(System.in);
+        int n = scClassWork.nextInt();
+        int res = 0;
+        for (int i = 0; i <= n; i++) {
+                res+=i;
+        }
+        System.out.println(res);
 //        ***
 //        for (int i = 0; i < 5; i++) {
 //            System.out.println(i + "|");
@@ -23,11 +23,11 @@ public class ClassWork {
 //        for (; ; ) {
 //            System.out.println("this is an infinite loop");
 //            }
-        int x = 0;
-        long y = 10;
-        for (y = 0, x = 1; x < 5; x++) {
-            System.out.println(x + " ");
-        }
+//        int x = 0;
+//        long y = 10;
+//        for (y = 0, x = 1; x < 5; x++) {
+//            System.out.println(x + " ");
+//        }
 
     }
 }
--- src/lesson3/Task_2.java
+++ src/lesson3/Task_2.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class Task_2 {
     public static void main(String[] args) {
         Scanner scTask_2 = new Scanner(System.in);
-                for (int result, a = scTask_2.nextInt(),b =scTask_2.nextInt(); ; ) {
 
+        int a = scTask_2.nextInt();
+        int b = scTask_2.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= b; ) {
+            i += 1;
+            sum += a;
+            if (i == b) {
+                System.out.println(sum);
+            }
 
         }
     }
