e1ed82a3d72cb67af4528706a7c74855f25ee859
xikari <xikari.98@mail.ru>
2022-01-28 21:54:45 +0300

    initial commit

---
+++ src/lesson4/Task_3.java
@@ -0,0 +1,15 @@
+package lesson4;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        int num= 1;
+        int sum=0;
+        do {
+//            System.out.print(num+" ");
+            sum+=num;
+//            System.out.println(sum+" ");
+            num+=2;
+        }while (num<=99);
+        System.out.println(sum);
+    }
+}
