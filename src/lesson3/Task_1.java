df0e112e1489f480b1577764e89bfe968ac4f433
xikari <xikari.98@mail.ru>
2022-01-28 22:53:07 +0300

    initial commit

--- src/lesson3/Task_1.java
+++ src/lesson3/Task_1.java
@@ -3,8 +3,10 @@ package lesson3;
 public class Task_1 {
     public static void main(String[] args) {
         //why did you put amoeba in for? do we need to cycle this variable? Or use inner for?
-        for (int amoeba = 1, time = 0; time <= 24; time += 3) {
-            System.out.println(amoeba *=2);
+        int amoeba = 1;
+        for (int time = 0; time <= 24; time += 3) {
+            amoeba *= 2;
         }
+        System.out.println(amoeba);
     }
 }
