4f2fff70673d6dd54c651f216d6180d948aa3f07
xikari <xikari.98@mail.ru>
2022-01-28 21:46:36 +0300

    initial commit

---
+++ src/lesson4/Task_2.java
@@ -0,0 +1,11 @@
+package lesson4;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int num = 0;
+        do{
+            num+=2;
+            System.out.print(num+" ");
+        }while (num<100);
+    }
+}
