29959a866037b2f4f0820fe8a499856fd8aa33e5
xikari <xikari.98@mail.ru>
2022-01-28 23:22:23 +0300

    initial commit

--- src/lesson4/Task_4.java
+++ src/lesson4/Task_4.java
@@ -2,22 +2,20 @@ package lesson4;
 
 public class Task_4 {
     public static void main(String[] args) {
-        double evenNumbers = 0;
-        double notEvenNumbers = 1;
-        double sumEvenNum = 0;
-        double sumNotEvenNum = 0;
-        do {
-            sumEvenNum += evenNumbers;
-            evenNumbers += 2;
-        } while (evenNumbers < 100 & evenNumbers % 2 == 0);
+        int sumEvenNum = 0;
+        int sumNotEvenNum = 0;
+        int num = 0;
+
+        while (num < 100) {
+            num += 1;
+            if (num % 2 == 0) {
+                sumEvenNum += num;
+            } else {
+                sumNotEvenNum += num;
+            }
+        }
         System.out.println("Сумма четных чисел " + sumEvenNum);
-
-        do {
-            sumNotEvenNum += notEvenNumbers;
-            notEvenNumbers += 2;
-        } while (notEvenNumbers <= 99 & notEvenNumbers % 2 != 0);
         System.out.println("Сумма нечетных чисел " + sumNotEvenNum);
-
         System.out.println("Общая сумма " + (sumEvenNum + sumNotEvenNum));
     }
 }
