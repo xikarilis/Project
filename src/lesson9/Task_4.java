9f2e04a3d09ed9a4b4a4676ecd1e4801d4be235a
xikari <xikari.98@mail.ru>
2022-02-17 20:48:14 +0300

    initial commit

---
+++ src/lesson9/Task_4.java
@@ -0,0 +1,23 @@
+package lesson9;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("enter size array");
+        int size = scanner.nextInt();
+        int[][] myArray = new int[size][size];
+        int[][] result = new int[size][1];
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray.length; j++) {
+                myArray[i][j] = random.nextInt(20);
+                result[i][0] += myArray[i][j];
+            }
+            System.out.println(Arrays.toString(myArray[i]) + Arrays.toString(result[i]));
+        }
+    }
+}
