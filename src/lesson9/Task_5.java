d33df2c9cd9e6e540d76ad0cb383ae2434f81415
xikari <xikari.98@mail.ru>
2022-02-17 21:42:16 +0300

    initial commit

---
+++ src/lesson9/Task_5.java
@@ -0,0 +1,26 @@
+package lesson9;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("enter size array");
+        int size = scanner.nextInt();
+        int[][] myArray = new int[size][size];
+        int result = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray.length; j++) {
+                myArray[i][j] = random.nextInt(20);
+            }
+            System.out.println(Arrays.toString(myArray[i]));
+        }
+        for (int i = 0, j = 0; i < myArray.length & i == j; i++, j++) {
+            result += myArray[i][j];
+        }
+        System.out.println(result);
+    }
+}
