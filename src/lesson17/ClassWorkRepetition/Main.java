00e1b34ef7f94fa2e69026b7149b39053ed528c6
xikari <xikari.98@mail.ru>
2022-03-23 17:36:36 +0300

    counting the number of object matches

--- src/lesson17/ClassWorkRepetition/Main.java
+++ src/lesson17/ClassWorkRepetition/Main.java
@@ -1,14 +1,12 @@
 package lesson17.ClassWorkRepetition;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Person person = new Person();
         person.show();
-        Person[] personArray = {
+        Person[] people = {
                 new Person("Max", 15),
                 new Person("Max", 15),
                 new Person("Dima", 14),
@@ -16,17 +14,26 @@ public class Main {
                 new Person("Max", 15),
                 new Person("Dima", 14),
                 new Person("Max", 15),
-                new Person("Max", 15)};
-        System.out.println(Arrays.toString(personArray));
-        int count = 0;
-        {
-            Set set = new HashSet();
-            for (int i = 0; i < personArray.length; i++) {
-                if (!set.add(personArray[i])) {
-                    count++;
+                new Person("Max", 15),
+                new Person()};
+        System.out.println(Arrays.toString(people));
+
+        boolean[] isAccounted = new boolean[people.length];
+        int equalityCounter = 0;
+        for (int i = 0; i < people.length; i++) {
+            if (isAccounted[i]) {
+                continue;
+            }
+            for (int j = i + 1; j < people.length; j++) {
+                if (isAccounted[i]) {
+                    continue;
+                }
+                if (people[i].equals(people[j])) {
+                    equalityCounter++;
+                    isAccounted[j] = true;
                 }
             }
-            System.out.println(count);
         }
+        System.out.println(equalityCounter);
     }
 }
